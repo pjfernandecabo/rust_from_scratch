d5795aa41b42171aec597a6176c0c8ca948d9061
pjfernandecabo <pedro.fdc@gmail.com>
2025-11-17 18:09:37 +0100

    adding files from 00-lesson

---
+++ 00_intro/target/debug/deps/rust_beginner-89376711a4dc5443.d
@@ -0,0 +1,5 @@
+/Users/pedro/Projects/rust_beginner/00_intro/target/debug/deps/rust_beginner-89376711a4dc5443.d: src/main.rs
+
+/Users/pedro/Projects/rust_beginner/00_intro/target/debug/deps/librust_beginner-89376711a4dc5443.rmeta: src/main.rs
+
+src/main.rs:
