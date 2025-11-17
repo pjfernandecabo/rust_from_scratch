a20fd0614aac70e18396e85b4291020bdde63c32
pjfernandecabo <pedro.fdc@gmail.com>
2025-11-17 18:00:01 +0100

    lesson 02 completed

---
+++ lesson_02_functions/target/debug/deps/lesson_02_functions-7848cfe1d9630950.d
@@ -0,0 +1,5 @@
+/Users/pedro/Projects/rust_beginner/lesson_02_functions/target/debug/deps/lesson_02_functions-7848cfe1d9630950.d: src/main.rs
+
+/Users/pedro/Projects/rust_beginner/lesson_02_functions/target/debug/deps/liblesson_02_functions-7848cfe1d9630950.rmeta: src/main.rs
+
+src/main.rs:
